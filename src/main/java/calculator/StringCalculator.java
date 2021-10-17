f43e09e7b6f76e293c6b2a7c4f51a1d77bacad78
Gaurav Dhabale <gauravdhabale77727@gmail.com>
2021-10-17 22:39:51 +0530

    add method for given numbers

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,8 +2,16 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-        return 0;
-    }
+	public int add(String input) {
+		int sum = 0;
+		String[] arr=input.	split(",");
+		if(input.contains(";") && arr.length==2) {
+    		for(int i=0; i<arr.length; i++) {
+    			sum=sum+Integer.parseInt(arr[i]);
+    		}
+    		
 
+}
+		return sum;
+}
 }
