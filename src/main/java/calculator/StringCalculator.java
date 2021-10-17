e269373bae04660afd5d1b80fbd30ee2d97191b1
Gaurav Dhabale <gauravdhabale77727@gmail.com>
2021-10-17 22:44:01 +0530

    for only one number in the string

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,13 +5,17 @@ class StringCalculator {
 	public int add(String input) {
 		int sum = 0;
 		String[] arr=input.	split(",");
+		// if given string contains only two numbers
 		if(input.contains(";") && arr.length==2) {
     		for(int i=0; i<arr.length; i++) {
     			sum=sum+Integer.parseInt(arr[i]);
     		}
-    		
-
+    		return sum;
+		}else if(input.length()>=1) {
+	    		return Integer.parseInt(input);
 }
-		return sum;
+		 else {
+	    		return 0;
 }
+	}
 }
