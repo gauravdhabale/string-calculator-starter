4c41148e88b405682e51a12620c2c0a2fafce57f
Gaurav Dhabale <gauravdhabale77727@gmail.com>
2021-10-17 22:46:32 +0530

    comments added

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,6 +5,7 @@ class StringCalculator {
 	public int add(String input) {
 		int sum = 0;
 		String[] arr=input.	split(",");
+		// for string contains more than two numbers
 		if(input.contains(";") && arr.length==2) {
     		for(int i=0; i<arr.length; i++) {
     			sum=sum+Integer.parseInt(arr[i]);
@@ -16,9 +17,12 @@ class StringCalculator {
     			sum=sum+Integer.parseInt(arr[i]);
     		}
     		return sum;
-		}else if(input.length()>=1) {
+		}
+		// for string contains only one number
+		else if(input.length()>=1) {
 	    		return Integer.parseInt(input);
 }
+		// if string is empty
 		 else {
 	    		return 0;
 }
